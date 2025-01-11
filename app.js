3f5667f54c488dc01e08104cae12f979662a5752
tantaihaha4487 <tantaiiottwo@gmail.com>
2025-01-11 16:40:50 +0700

    initailize project

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+    }
+);
