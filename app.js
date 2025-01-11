70749ce421ac3b8c3d0e3b3eed8c9f9a4c432435
tantaihaha4487 <tantaiiottwo@gmail.com>
2025-01-11 17:36:34 +0700

    Implemented a dropzone for file uploads with drag and drop functionality.

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.set('view engine', 'ejs');
-
 
 app.get('/', (req, res) => {
   res.render('index.ejs');
 });
 
-
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-    }
-);
+  console.log('Server is running on port 3000');
+});
