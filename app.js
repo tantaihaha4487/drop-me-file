cf17e0e976202fc8a5ce97edcdc82e606c09263f
tantaihaha4487 <tantaiiottwo@gmail.com>
2025-01-14 00:04:17 +0700

    [Modify] /upload now handle when upload folder doesn't exits. Webpag now report upload status

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
+
+uploadPath = path.join(__dirname, 'uploads');
 
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/');
+    cb(null, uploadPath);
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + '-' + file.originalname);
@@ -22,10 +25,15 @@ app.get('/', (req, res) => {
 
 
 app.post('/upload', (req, res) => {
+  // If upload path does not exist, create it!
+  if (!fs.existsSync(uploadPath)) {
+    fs.mkdirSync(uploadPath);
+  }
+
   const upload = multer({ storage: storage }).single('file');
   upload(req, res, (err) => {
-    if (err) return res.send(err);
-    res.send('File uploaded successfully!');
+    if (err) return res.status(400).json({ message: 'Something went wrong!' });
+    res.status(200).json({ message: 'File uploaded successfully.' });
   });
 });
 
