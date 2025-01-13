500f9abc8b484dc66e4cd0243fa48bc11f42a07f
tantaihaha4487 <tantaiiottwo@gmail.com>
2025-01-13 23:36:26 +0700

    [Add] Create upload file at /upload  route

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
 const express = require('express');
-const app = express();
+const multer = require('multer');
 const path = require('path');
 
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+});
+
+const app = express();
 app.set('view engine', 'ejs');
+
 
 app.get('/', (req, res) => {
   res.render('index.ejs');
 });
+
+
+app.post('/upload', (req, res) => {
+  const upload = multer({ storage: storage }).single('file');
+  upload(req, res, (err) => {
+    if (err) return res.send(err);
+    res.send('File uploaded successfully!');
+  });
+});
+
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
