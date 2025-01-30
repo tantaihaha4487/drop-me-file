eee966d767ce6ed52da5ce34cd09761b5aac43dd
tantaihaha4487 <tantaiiottwo@gmail.com>
2025-01-30 23:18:38 +0700

    [Add] add return button in displat.ejs

--- routes/display.route.js
+++ routes/display.route.js
@@ -6,8 +6,11 @@ const uploadPath = path.join(__dirname, '../uploads');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  const files = fs.readdirSync(uploadPath);
-  if (!(files.length > 0)) return res.render('error.ejs', { message: "No Files Found", error: "Sorry, no files found in the uploads directory. Please upload any file first." });
+  // Check if the uploads directory exists
+  if (!fs.existsSync(uploadPath)) fs.mkdirSync(uploadPath);
+  const files = fs.readdirSync(uploadPath)
+  // Check if there are no files in the uploads directory
+  if (files.length === 0) return res.render('error.ejs', { message: "No Files Found", error: "Sorry, no files found in the uploads directory. Please upload any file first." });
   res.render('display.ejs', { files: files });
 });
 
